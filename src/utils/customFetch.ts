fb1da5dc2839a7f6435888eb15f39962b1b3c6bf
Kusuma Sandi <wijayakusumasandi@gmail.com>
2022-12-01 17:02:05 +0800

    Fix: Map componen rerender

--- src/utils/customFetch.ts
+++ src/utils/customFetch.ts
@@ -1,7 +1,6 @@
 const base_url = process.env.REACT_APP_BASE_URL;
 const customFetch = async (url: string) => {
   const access_token = localStorage.getItem("access_token");
-  const refresh_token = localStorage.getItem("refresh_token");
 
   const data = await fetch(`${base_url + url}`, {
     method: "GET",
@@ -12,28 +11,29 @@ const customFetch = async (url: string) => {
   })
     .then((res) => {
       if (res.status === 401) {
-        const status = fetch(`${base_url}/auth/refreshToken`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            refresh_token,
-          }),
-        })
-          .then((res) => res.json())
-          .then((result) => {
-            localStorage.setItem("access_token", `${result.data.access_token}`);
-            localStorage.setItem(
-              "refresh_token",
-              `${result.data.refresh_token}`
-            );
-          })
-          .then(() => {
-            return "token_expired";
-          });
+        window.location.href = "/";
+        // const status = fetch(`${base_url}/auth/refreshToken`, {
+        //   method: "POST",
+        //   headers: {
+        //     "Content-Type": "application/json",
+        //   },
+        //   body: JSON.stringify({
+        //     refresh_token,
+        //   }),
+        // })
+        //   .then((res) => res.json())
+        //   .then((result) => {
+        //     localStorage.setItem("access_token", `${result.data.access_token}`);
+        //     localStorage.setItem(
+        //       "refresh_token",
+        //       `${result.data.refresh_token}`
+        //     );
+        //   })
+        //   .then(() => {
+        //     return "token_expired";
+        //   });
 
-        return status;
+        // return status;
       } else {
         return res.json();
       }
